2fe78eec20ecaf11d014c9e8a79f024d8014fd89
Igor-Mont <igornathandev@gmail.com>
2023-01-25 10:54:52 -0300

    Bin e Hash

---
+++ ufs/hashing/tabelaHash.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <string.h>
+
+// 2 * 15 (qtd. elementos) = primo mais proximo da soma
+
+#define TAM 31
+
+void inicializarTabela(int t[]) {
+  for(int i = 0; i < TAM; i++)
+    t[i] = 0;
+} 
+
+int hash(int chave) {
+  return chave % TAM;
+}
+
+void inserir(int valor, int t[]) {
+  int id = hash(valor);
+
+  while(t[id] != 0) {
+    id = hash(id + 1);
+  } 
+
+  t[id] = valor;
+}
+
+int busca(int t[], int chave) {
+  int id = hash(chave);
+
+  printf("Chave gerada> %d\n", id);
+
+  while(t[id] != 0) {
+    if(t[id] == chave) return t[id];
+
+    id = hash(id + 1);
+  }
+
+  return 0;
+}
+
+void imprimir (int t[]) {
+  for(int i = 0; i < TAM; i++) {
+    printf("%d = %d\n", i, t[i]);
+  }
+}
+
+int main() {
+  int opcao = 0;
+  int valor;
+  int tabela[TAM];
+
+  inicializarTabela(tabela);
+
+  do {
+    printf("\n\t0 - Sair");
+    printf("\n\t1 - Inserir");
+    printf("\n\t2 - Buscar");
+    printf("\n\t3 - Imprimir");
+
+    scanf("%d", &opcao);
+
+    switch (opcao) {
+    case 1:
+      printf("\t Qual valor deseja inserir? ");
+      scanf("%d", &valor);
+      inserir(valor, tabela);
+      break;
+    case 2:
+      printf("\t Qual valor deseja buscar? ");
+      scanf("%d", &valor);
+      int retorno = busca(tabela, valor);
+      if(retorno != 0) {
+        printf("Valor encontrado: %d\n", retorno);
+      } else {
+        printf("Valor não encontrado\n");
+      }
+      break;
+    case 3:
+      imprimir(tabela);
+      break;
+    
+    default:
+      printf("Opcao invalida");
+    }
+  } while(opcao != 0);
+
+  return 0;
+}
---
+++ ufs/lista-sequencial-ordenada-busca-bin/buscaBinaria.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+
+bool buscaBinaria(int l[20], int size, int num) {
+  int begin = 0;
+  int end = size - 1;
+
+  if(l[begin] == num) return true;
+  if(l[end] == num) return true;
+
+  while (begin <= end) { 
+      int middle = (begin + end) / 2;
+
+      if (l[middle] == num) return true;
+
+      if (l[middle] < num)  
+          begin = middle + 1;
+
+      if (l[middle] > num)
+          end = middle;
+  }
+
+  return false;
+}
+
+int main() {
+  int vector[28] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28};
+
+  double time_spent = 0.0;
+  clock_t begin = clock();
+
+  bool find = buscaBinaria(vector, 28, 28);
+
+  clock_t end = clock();
+  time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+
+  printf("The elapsed time is %f seconds\n", time_spent);
+
+  printf(find ? "Find!": "Bad!");
+
+
+  return 0;
+}
